30f8cd6957368fa261b61604581b842bff1e63db
kanamycine <jinyeong5320@gmail.com>
2020-08-14 18:16:22 +0900

    SWEA : 1979

--- SWEA/D2/[D2] 1979. 어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/[D2] 1979. 어디에 단어가 들어갈 수 있을까.py
@@ -1,27 +1,32 @@
 tc = int(input())
 for t in range(tc):
-    a, b = map(int, input().split())
-    
-    
-    input_list = [[0] * a for _ in range(a)]
-    
+    N, K = map(int, input().split())
 
-    for a in range(a):
+    input_list = [[0] * N for _ in range(N)]
+
+    for a in range(N):
         input_list[a] = list(map(int, input().split()))
-    
+
     ans = 0
-    for b in range(a): 
-        rowcnt = 0 
+    for b in range(N):
+        rowcnt = 0
         colcnt = 0
-        for c in range(a):
+        for c in range(N):
             if input_list[b][c] == 1:
                 rowcnt += 1
             elif input_list[b][c] == 0:
-                if rowcnt == b:
+                if rowcnt == K:
                     ans += 1
-                rowcnt = 0   
-            
+                rowcnt = 0
+            if input_list[c][b] == 1:
+                colcnt += 1
+            elif input_list[c][b] == 0:
+                if colcnt == K:
+                    ans += 1
+                colcnt = 0
+        if rowcnt == K:
+            ans += 1
+        if colcnt == K:
+            ans += 1
 
-
-
-
+    print('#%d %d' % (t+1, ans))
